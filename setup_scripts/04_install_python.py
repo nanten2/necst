cb20ffd755a92345f8fc20ee1388d54e4d749a46
Atsushi Nishimura <ars096@gmail.com>
2017-10-13 16:08:55 +0900

    add install python

---
+++ setup_scripts/04_install_python.py
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+sudo apt-get install -y make
+sudo apt-get install -y build-essential
+sudo apt-get install -y openssl
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libbz2-dev
+sudo apt-get install -y wget
+sudo apt-get install -y curl
+sudo apt-get install -y llvm
+sudo apt-get install -y libreadline-dev
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y libncurses5-dev
+sudo apt-get install -y libncursesw5-dev
+sudo apt-get install -y libpng-dev
+
+

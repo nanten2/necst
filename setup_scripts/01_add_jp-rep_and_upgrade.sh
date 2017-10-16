47cc7dc5daa27032d17bc8f7f19a6a573a6633dc
Atsushi Nishimura <ars096@gmail.com>
2017-10-16 12:42:04 +0900

    copy original sources.list file

--- setup_scripts/01_add_jp-rep_and_upgrade.sh
+++ setup_scripts/01_add_jp-rep_and_upgrade.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.original
 sudo sed -i.bak -e "s%http://[^ ]\+%http://ftp.jaist.ac.jp/pub/Linux/ubuntu/%g" /etc/apt/sources.list
 
 sudo apt-get update

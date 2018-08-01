9dc990d4071469fc3b3832ad7f79a2f51cba94fd
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-08-01 13:53:45 +0900

    Update 13_setup_necst.sh
    1 回目の source は $HOME/ros/devel/setup.bash でエラーが出ますが気にしないでください。

--- setup_scripts/13_setup_necst.sh
+++ setup_scripts/13_setup_necst.sh
@@ -9,6 +9,11 @@ cd $HOME/ros
 catkin_make
 
 git clone https://github.com/nanten2/necst-ros.git $HOME/ros/src/necst
+git clone https://github.com/nanten2/NASCORX_XFFTS.git $HOME/ros/src/nascorx_xffts
+
+source ~/.bashrc
 
 cd $HOME/ros
 catkin_make
+
+source ~/.bashrc

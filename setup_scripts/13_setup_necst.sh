bdf27c3c201d7ec4349196fd3c6a19e686e11f96
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-08-08 14:31:40 +0900

    Update 13_setup_necst.sh

--- setup_scripts/13_setup_necst.sh
+++ setup_scripts/13_setup_necst.sh
@@ -7,10 +7,6 @@ cd $HOME/ros/src
 catkin_init_workspace
 cd $HOME/ros
 
-source $HOME/.bashrc
 git clone https://github.com/nanten2/necst-ros.git $HOME/ros/src/necst
-catkin_make
-
-source $HOME/.bashrc
 git clone https://github.com/nanten2/NASCORX_XFFTS.git $HOME/ros/src/nascorx_xffts
 catkin_make
